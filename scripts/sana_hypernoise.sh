bbe3ab4f9e2df89d4ccdd468acdd5246afde5e56
Luca Eyring <luca.eyring@googlemail.com>
2025-08-15 13:55:39 +0200

    initial_commit

--- scripts/sana_hypernoise.sh
+++ scripts/sana_hypernoise.sh
@@ -7,7 +7,7 @@ export OMP_NUM_THREADS=12
 
 python -m torch.distributed.run \
     --nnodes=1 \
-    --nproc_per_node=1 \
+    --nproc_per_node=6 \
     --master_port=25811 \
     main.py \
     --task="all" \
@@ -17,14 +17,14 @@ python -m torch.distributed.run \
     --alpha_multiplier=2 \
     --optim="sgd" \
     --lr=1e-3 \
-    --batch_size=3 \
+    --batch_size=18 \
     --accumulation_steps=3 \
     --grad_clip=1.0 \
     --warmup_steps=100 \
     --reg_weight=0.5 \
     --latent_type="batch" \
-    --epochs=5 \
+    --epochs=30 \
     --save_dir="./outputs" \
-    --log_every=10 \
-    --save_every=10 \
+    --log_every=5000 \
+    --save_every=25000 \
     --memsave
